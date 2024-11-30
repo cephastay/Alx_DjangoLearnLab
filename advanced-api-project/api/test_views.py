d4cad737b9dab5eccb0fa5b7b3f81d51955eb861
Cephas Tay <cephasmc3.17@gmail.com>
2024-11-30 13:32:45 +0000

    imported rest framework status codes instead of hardcoding

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,5 +1,6 @@
 from rest_framework.test import APITestCase
 from rest_framework.reverse import reverse
+from rest_framework import status
 
 
 class LoginViewTestCase(APITestCase):
@@ -16,12 +17,12 @@ class LoginViewTestCase(APITestCase):
 
     def test_login_page_get(self):
         response = self.client.get(self.login_url)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_booklist_page_get(self):
         response = self.client.get(self.books_list_url)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_authorlist_page_get(self):
         response = self.client.get(self.authors_list_url)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
