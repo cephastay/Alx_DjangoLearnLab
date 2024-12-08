b31c6a522442a0377624fa488a464b40eedcdb7f
Cephas Tay <cephasmc3.17@gmail.com>
2024-12-08 07:37:56 +0000

    configured url paths to match checker pattern

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -19,8 +19,8 @@ urlpatterns = [
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
     path('post/', views.PostListView.as_view(), name='posts'),
 
-    path('comments/', views.CommentListView.as_view(),name='comment-list'),
-    path('comments/<int:pk>/edit/', views.CommentUpdateView.as_view(),name='comment-update'),
-    path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(),name='comment-delete'),
+    path('comment/', views.CommentListView.as_view(),name='comment-list'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(),name='comment-update'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(),name='comment-delete'),
     path('post/<int:post_id>/comments/new/', views.commentdummy, name='comment-dummy'),
 ]
