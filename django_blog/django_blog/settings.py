b2aa492afc5bfc69fae54ad34ed130509b1ba097
Cephas Tay <cephasmc3.17@gmail.com>
2024-12-08 00:33:57 +0000

    configured env file for project settings

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -20,12 +20,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jdvsng*$e7#1w9607$9h@x6!nubu=^@t4ch$0xhe)w$b81w4*d'
+from decouple import config, Csv
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost'
+]
 
 
 # Application definition
